6afb3038c4597ef7f1c90828b038afd4f937a00b
qi55wyqu <jonas.parchent@gmail.com>
2017-05-07 21:18:56 +0200

    Origin-Berechnung korrigiert

--- src/edu/stanford/rsl/tutorial/qi55wyqu/Phantom.java
+++ src/edu/stanford/rsl/tutorial/qi55wyqu/Phantom.java
@@ -16,8 +16,8 @@ public class Phantom extends Grid2D {
 		double[] origin = new double[2];
 		for (int i = 0; i < 2; i++) {
 			// World origin in image center
-			origin[i] = -(size[i] - 1) * spacing[i];
-		}
+			origin[i] = -(size[i] - 1) * 0.5 * spacing[i];
+		} 
 		this.setOrigin(origin);
 		
 		this.drawEllipse(
