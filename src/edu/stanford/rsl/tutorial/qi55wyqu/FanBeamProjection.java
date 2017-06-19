aa360bb62a6248e2a1b09849adedd683ebe0e19f
Jonas Parchent <qi55wyqu@stud.informatik.uni-erlangen.de>
2017-06-19 20:01:05 +0200

    fanbeam

--- src/edu/stanford/rsl/tutorial/qi55wyqu/FanBeamProjection.java
+++ src/edu/stanford/rsl/tutorial/qi55wyqu/FanBeamProjection.java
@@ -1,6 +1,7 @@
 package edu.stanford.rsl.tutorial.qi55wyqu;
 
 import edu.stanford.rsl.conrad.data.numeric.Grid2D;
+import edu.stanford.rsl.conrad.geometry.shapes.simple.Point2D;
 import edu.stanford.rsl.conrad.utils.VisualizationUtil;
 import ij.ImageJ;
 import ij.ImagePlus;
@@ -8,7 +9,8 @@ import ij.ImagePlus;
 public class FanBeamProjection {
 	
 	protected int numProjections, numDetectorPixels;
-	protected double detectorSpacing, numDetectorElements, sampleSpacing, angularIncrement, distSourceIso, distSourceDet;
+	protected double detectorSpacing, angularIncrement, distSourceIso, distSourceDet;
+	protected double sampleSpacing, detectorLength, gamma, rotationDegress, distDetIso;
 
 	public FanBeamProjection(
 		double detectorSpacing, 
@@ -18,23 +20,55 @@ public class FanBeamProjection {
 		double distSourceIso, 
 		double distSourceDet) {
 			
-		this.detectorSpacing = detectorSpacing;
+		this.detectorSpacing   = detectorSpacing;
 		this.numDetectorPixels = numDetectorPixels;
-		this.angularIncrement = angularIncrement;
-		this.numProjections = numProjections;
-		this.distSourceIso = distSourceIso;
-		this.distSourceDet = distSourceDet;
+		this.angularIncrement  = angularIncrement;
+		this.numProjections    = numProjections;
+		this.distSourceIso     = distSourceIso;
+		this.distSourceDet     = distSourceDet;
+		
+		this.initialise();
+	}
+	
+	private void initialise() {
 		this.sampleSpacing = 0.5;
+		this.detectorLength = this.numDetectorPixels * this.detectorSpacing;
+		this.gamma = Math.atan(0.5 * this.detectorLength / this.distSourceDet);
+		this.rotationDegress = this.numProjections * this.angularIncrement;
+		this.distDetIso = this.distSourceDet - this.distSourceIso;
 	}
 	
 	public Grid2D project(Grid2D inputImage) {
+
+		double diag = Math.sqrt(
+			  0.5 * inputImage.getWidth()  * inputImage.getSpacing()[0] 
+			+ 0.5 * inputImage.getHeight() * inputImage.getSpacing()[1]
+		);
+		if (this.distSourceIso <= diag) {
+			System.err.println("Tube will hit object!");
+		}
+		else if (this.distDetIso <= diag) {
+			System.err.println("Detector will hit object!");
+		}
 		
 		Grid2D fanogram = new Grid2D(this.numDetectorPixels, this.numProjections);
 		fanogram.setSpacing(new double[] { this.detectorSpacing, this.angularIncrement });
 		fanogram.setOrigin(new double[] { -(this.numDetectorPixels - 1) * this.detectorSpacing / 2, 0.0 });
 		
-		
-		
+		for (int betaIndex = 0; betaIndex < this.rotationDegress; betaIndex++) {
+			
+			double beta = betaIndex * this.angularIncrement;
+			double sourcePosX = Math.cos(Math.toRadians(beta)) * this.distSourceIso;
+			double sourcePosY = Math.sin(Math.toRadians(beta)) * this.distSourceIso;
+			Point2D sourcePos = new Point2D(new double[] { sourcePosX, sourcePosY });
+			
+			for (int tIndex = 0; tIndex < this.numDetectorPixels; tIndex++) {
+				
+				
+				
+			}
+		}
+				
 		return fanogram;
 		
 	}
