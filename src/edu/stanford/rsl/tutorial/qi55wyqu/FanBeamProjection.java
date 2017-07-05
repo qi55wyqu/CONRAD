5b696a98ff2aac3bfa748b62f1a0530c51f87208
qi55wyqu <jonas.parchent@gmail.com>
2017-07-06 01:45:19 +0200

    Rebinning

--- src/edu/stanford/rsl/tutorial/qi55wyqu/FanBeamProjection.java
+++ src/edu/stanford/rsl/tutorial/qi55wyqu/FanBeamProjection.java
@@ -148,7 +148,7 @@ public class FanBeamProjection {
 				double beta = theta - gamma;
 				double[] fanoIdx = fanogram.physicalToIndex(t, beta);
 				if (fanoIdx[1] > fanogram.getHeight()) {
-					fanoIdx[1] -= 180; // erst in physische koordinaten umrechnen!!!
+					fanoIdx[1] -= 180;
 				}
 				float val = InterpolationOperators.interpolateLinear(fanogram, fanoIdx[0], fanoIdx[1]);
 				sinogram.setAtIndex(sIndex, thetaIndex, val);
